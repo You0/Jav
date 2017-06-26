86251779368d2b3459d2ee19b22d22b12f1ff060
You0 <1109245765@qq.com>
2017-06-26 16:48:46 +0800

    回家更新
    回家更新

--- src/test/java/com/me/Jav/TestMyBatis.java
+++ src/test/java/com/me/Jav/TestMyBatis.java
@@ -1,37 +1,14 @@
 package com.me.Jav;
 
-  
-import javax.annotation.Resource;  
-  
-import org.apache.log4j.Logger;  
-import org.junit.Before;  
-import org.junit.Test;  
-import org.junit.runner.RunWith;  
-import org.springframework.context.ApplicationContext;  
-import org.springframework.context.support.ClassPathXmlApplicationContext;  
-import org.springframework.test.context.ContextConfiguration;  
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.junit.Test;
 
-import com.me.service.ITestService;  
-
-  
-@RunWith(SpringJUnit4ClassRunner.class)     //表示继承了SpringJUnit4ClassRunner类  
-@ContextConfiguration(locations = {"classpath:spring-mybatis.xml"})  
-  
 public class TestMyBatis {  
 
-    private ApplicationContext ac = null;  
-    private ITestService userService;
-  @Before  
-  public void before() {  
-      ac = new ClassPathXmlApplicationContext("applicationContext.xml");  
-      userService = (ITestService) ac.getBean("userService");  
-  }  
+
   
     @Test  
     public void test1() {  
-        com.me.entity.Test user = userService.getTestById(213);  
-        // System.out.println(user.getUserName());  
+        System.out.println("test");
 
     }  
 }  
